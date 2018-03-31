d7ab9d1d80f92dface0bfa79d6f2af391b1cb77d
Alex Orga <alexorga.dev@gmail.com>
2018-03-30 21:01:58 -0500

    ao-16-startAppFunction

---
+++ main.js
@@ -0,0 +1,7 @@
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", fileLoaded);
+    myRequest.addEventListener("error", fileError);
+    myRequest.open("GET", "planets.json");
+    myRequest.send();
+}
